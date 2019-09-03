3967f0f79d3cfff396cbf8e55c22d764aa45bd19
Laboratório de Redes <labredes@Labredes.pucrs.br>
2019-09-02 22:17:02 -0300

    adding packages

--- model/Acoes.java
+++ model/Acoes.java
@@ -1,3 +1,4 @@
 public class Acoes{
+    public String nome;
     
 }
---
+++ model/Objeto.java
@@ -0,0 +1,6 @@
+public class Objeto{
+    public String tipo;
+    public List<Acoes> lstAcoes = new ArrayList<>();
+    
+    
+}
